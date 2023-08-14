337f2d415dc3a6e43b8e4dce75f310d8050d6c68
Bryan Camarillo <bryancamarillo91@gmail.com>
2023-08-14 14:01:26 -0700

    able to grab the byline and date from the article

--- work.py
+++ work.py
@@ -10,6 +10,13 @@ parText = ""
 for e in par: # for loops that looks through the list and extracts all of the text from the list 
     parText = parText + e.get_text()
 
-print(title)
-print(parText)
+date = soup.find('meta', property='article:modified_time')['content'] #looks at the meta tag and grabs the date the article was posted
+
+byline = soup.find('meta', attrs ={'name': 'author'}) # grabs the authors name from the meta tag 
+byline = byline['content']
+
+print("Date published: ", date)
+print("Byline: ", byline)
+print("Title: ", title)
+print("Article Content: ", parText)
 
