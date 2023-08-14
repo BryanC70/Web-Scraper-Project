2ee0a4f371b56f7d2a81b8249768b55a67d1f18d
Bryan Camarillo <bryancamarillo91@gmail.com>
2023-08-14 13:21:23 -0700

    able to get the bulk of the article, now working on byline

--- work.py
+++ work.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html')
 soup = BeautifulSoup(r.content, 'html.parser')
-title = soup.title
-header = soup.header
+title = soup.title.get_text() # grabs the title from the article
+
+par = soup.find_all('p') # looks through article and finds all instances of a "p" tag and puts them in a list
+parText = ""
+for e in par: # for loops that looks through the list and extracts all of the text from the list 
+    parText = parText + e.get_text()
+
 print(title)
+print(parText)
+
