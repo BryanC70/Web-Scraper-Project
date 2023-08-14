4ff62eabe471bd4d15d61d1061d081bbaa1e3842
BryanC70 <108298449+BryanC70@users.noreply.github.com>
2023-08-14 14:03:40 -0700

    Update work.py
    Removes comments from the WIP branch

--- work.py
+++ work.py
@@ -3,16 +3,16 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://edition.cnn.com/travel/article/scenic-airport-landings-2020/index.html')
 soup = BeautifulSoup(r.content, 'html.parser')
-title = soup.title.get_text() # grabs the title from the article
+title = soup.title.get_text() 
 
-par = soup.find_all('p') # looks through article and finds all instances of a "p" tag and puts them in a list
+par = soup.find_all('p')
 parText = ""
-for e in par: # for loops that looks through the list and extracts all of the text from the list 
+for e in par: 
     parText = parText + e.get_text()
 
-date = soup.find('meta', property='article:modified_time')['content'] #looks at the meta tag and grabs the date the article was posted
+date = soup.find('meta', property='article:modified_time')['content'] 
 
-byline = soup.find('meta', attrs ={'name': 'author'}) # grabs the authors name from the meta tag 
+byline = soup.find('meta', attrs ={'name': 'author'}) 
 byline = byline['content']
 
 print("Date published: ", date)
